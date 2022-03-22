b3280a69802d7e70bd909ee048973881af7b2135
valenmads <101062243+valenmads@users.noreply.github.com>
2022-03-22 15:41:19 +0900

    C프로그래밍 4주차 2

---
+++ week4-2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main(void){
+	
+	char str[5];
+	
+	scanf("%c%c%c%c%c", &str[0],&str[1],&str[2],&str[3],&str[4]);
+	printf("%c%c%c%c%c\n",str[0],str[1],str[2],str[3],str[4]);
+}
