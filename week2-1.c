dae077e8d16901a1318defeac24d20804db21370
valenmads <101062243+valenmads@users.noreply.github.com>
2022-03-27 14:43:03 +0900

    C프로그래밍 2주차 1

---
+++ week2-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) {
+	int i, j, k = 1;
+	for (i = 1; i <= 5; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			printf(" %d", k++);
+		}
+		printf("\n");
+	}
+	return 0;
+}
